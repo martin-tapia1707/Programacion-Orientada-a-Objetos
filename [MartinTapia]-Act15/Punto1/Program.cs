1d7f707e20f263146c81dc634fd8f69488516ba0
martin-tapia1707 <tapiamartin590@gmail.com>
2026-05-29 11:47:04 -0300

    devin pomnisito de corason

---
+++ [MartinTapia]-Act15/Punto1/Program.cs
@@ -0,0 +1,97 @@
+﻿using Punto1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto1
+{
+
+    /*
+    1. Monitoreo Clínico de Pacientes
+    En una sala de cuidados intensivos, se requiere realizar un seguimiento constante del ritmo
+    cardíaco de los pacientes.
+    ● Plantear una clase llamada Paciente que tenga como atributos privados:
+    1. Nombre (de tipo string).
+    2. Una matriz llamada ritmoCardiaco de tamaño 3x4 (representando los
+    registros de pulsaciones tomados durante 3 días diferentes, en 4 momentos
+    clave de cada día: mañana, mediodía, tarde y noche).
+    3. El constructor de Paciente debe pedir su nombre y solicitar la carga por
+    teclado de las 12 lecturas cardíacas.
+
+    ● Confeccionar la clase colaboradora SalaMonitoreo que administre un vector de 3
+    objetos de la clase Paciente. Su constructor debe inicializar el vector y solicitar la
+    carga de los 3 pacientes. Implementar los siguientes métodos dentro de
+    SalaMonitoreo:
+    1. Un método que imprima la grilla de lecturas cardíacas completa de cada
+    paciente organizada por día y momento.
+    2. Un método que calcule e imprima el promedio de pulsaciones de cada
+    paciente a lo largo de todo su monitoreo.
+    3. Un método que verifique si algún paciente registró un evento de taquicardia
+    severa (cualquier lectura individual que sea estrictamente mayor a 120
+    pulsaciones), mostrando por consola su nombre y la lectura detectada. 
+    */
+
+    internal class Paciente
+    {
+
+        private string nombre;
+        private int[,] ritmoCardiaco;
+
+        public Paciente()
+        {
+            ritmoCardiaco = new int[3, 4];
+            Console.Write("Ingrese el nombre del paciente: ");
+            nombre = Console.ReadLine();
+            for (int f = 0; f < ritmoCardiaco.GetLength(0); f++)
+            {
+                Console.WriteLine("DIA " + (f+1));
+                for (int c = 0; c < ritmoCardiaco.GetLength(1); c++)
+                {
+                    Console.Write("Ingrese la " + (c + 1) + "° lectura cardiaca de " + nombre + ": ");
+                    ritmoCardiaco[f, c] = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+
+        public string DatoNombre()
+        {
+            return nombre;
+        }
+
+        public int[,] DatoRitCard()
+        {
+            return ritmoCardiaco;
+        }
+
+    }
+
+    internal class SalaMonitoreo
+    {
+
+        private Paciente[] paciente = new Paciente[3];
+
+        public SalaMonitoreo() 
+        { 
+            for(int i = 0; i < paciente.Length; i++)
+            {
+                paciente[i] = new Paciente();
+            }
+        }
+        
+    public void GrillaOrganizada()
+    {
+         // falta hacer guey
+    }
+
+        static void Main(string[] args)
+        {
+        SalaMonitoreo sm2 = new SalaMonitoreo();
+        sm2.GrillaOrganizada();
+
+        Console.ReadKey();
+        }
+    }
+}
