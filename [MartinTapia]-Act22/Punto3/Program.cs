fb1af2e30520ac94de325c2e92483d5802755816
Martín Tapia <tapiamartin590@gmail.com>
2026-07-03 11:34:58 -0300

    actividad 22 avances punto 3
    Added Entrenamiento class with methods to register training duration.

--- [MartinTapia]-Act22/Punto3/Program.cs
+++ [MartinTapia]-Act22/Punto3/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +6,39 @@ using System.Threading.Tasks;
 
 namespace Punto3
 {
-    internal class Program
+    /*
+    Actividad 3: Registro de entrenamientos
+    Problema:
+    Plantear una clase Entrenamiento con atributos: deportista y duración (en minutos).
+     Incluir dos métodos RegistrarDuracion (sobrecarga de métodos):
+    1. Uno que reciba horas y minutos y los convierta a minutos.
+    2. Otro que reciba directamente los minutos.
+     Crear una lista con 5 entrenamientos y mostrar el entrenamiento más largo y el
+    más corto. 
+    */
+    internal class Entrenamiento
     {
+        private string deportista;
+        private DateTime duracion;
+
+        public void RegistrarDuracion(DateTime Horas, DateTime Minutos)
+        {
+            duracion = (2, 20).TotalMinutes;
+
+        }
+
+        public void RegistrarDuracion(DateTime Minutos)
+        {
+
+        }
+
         static void Main(string[] args)
         {
+            Entrenamiento entrenamiento2 = new Entrenamiento();
+            entrenamiento2.RegistrarDuracion(DateTime.Today.AddHours(2), DateTime.Today.AddMinutes(20));
+            entrenamiento2.RegistrarDuracion(DateTime.Today.AddMinutes(40));
+
+            Console.ReadKey();
         }
     }
 }
