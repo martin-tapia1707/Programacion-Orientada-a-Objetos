cec36a3ddfae8b485b14b93ad0a158de2522bdd6
Martín Tapia <tapiamartin590@gmail.com>
2026-08-21 09:17:47 -0300

    act25 parte 2

---
+++ [MartinTapia]-Act25/Punto4/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Punto4
+{
+    /*
+    4. Elaborar una interfaz gráfica que muestre una calculadora (utilizar
+    objetos de la clase Button y un objeto de la clase Label donde se
+    muestra el valor ingresado), tener en cuenta que solo se debe
+    implementar la interfaz y la carga de un valor de hasta 12 dígitos. 
+    */
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if(label1.Text.Length <= 12)
+            {
+                label1.Text = label1.Text + "1";
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (label1.Text.Length < 12)
+            {
+                label1.Text = label1.Text + "2";
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if (label1.Text.Length < 12)
+            {
+                label1.Text = label1.Text + "3";
+            }
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            if (label1.Text.Length < 12)
+            {
+                label1.Text = label1.Text + "4";
+            }
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            if (label1.Text.Length < 12)
+            {
+                label1.Text = label1.Text + "5";
+            }
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            if (label1.Text.Length < 12)
+            {
+                label1.Text = label1.Text + "6";
+            }
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            if (label1.Text.Length < 12)
+            {
+                label1.Text = label1.Text + "7";
+            }
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            if (label1.Text.Length < 12)
+            {
+                label1.Text = label1.Text + "8";
+            }
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            if (label1.Text.Length < 12)
+            {
+                label1.Text = label1.Text + "9";
+            }
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            if (label1.Text.Length < 12)
+            {
+                label1.Text = label1.Text + "0";
+            }
+        }
+    }
+}
---
+++ [MartinTapia]-Act25/Punto5/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Punto5
+{
+    /*
+    5. Solicitar el ingreso de una clave de hasta 10 caracteres en un control de
+    tipo TextBox (inicializar la propiedad MaxLength con el valor 10)
+    Mostrar en un cuadro de mensajes la clave ingresada al presionar un
+    botón. 
+    */
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(textBox1.Text);
+        }
+    }
+}
